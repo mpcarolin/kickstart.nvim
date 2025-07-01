aee57f3bbdf0a338f73530ff43b62ba1ed088e1e
michael <mpcarolin.dev@gmail.com>
2025-07-01 09:27:14 -0700

    Add setup script

--- setup.sh
+++ setup.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
+CONFIG_DIR="$HOME/.config"
+NVIM_DIR="$CONFIG_DIR/nvim"
+
+# Setup config folder
+mkdir -p "$HOME/.config"
+
+# https://github.com/mpcarolin/kickstart.nvim 
+sudo add-apt-repository ppa:neovim-ppa/unstable -y
+sudo apt update
+sudo apt install make gcc ripgrep unzip git xclip neovim
+
 # Needed for some plugins, like rest.nvim
 sudo apt install luarocks
 
-# Installing docker
-# TODO:
-#
-## Add user to docker group so you don't need to use sudo with every command
-sudo usermod -aG docker $USER
+# clone the kickstart
+git clone https://github.com/mpcarolin/kickstart.nvim "$NVIM_DIR"
