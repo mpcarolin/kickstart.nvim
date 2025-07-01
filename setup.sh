d689b23e0a8dabc19bb9c62c11f74877fbafbacd
Michael Carolin <mpcarolin.dev@gmail.com>
2025-07-01 09:45:19 -0700

    Add yarn to setup script

--- setup.sh
+++ setup.sh
@@ -3,16 +3,18 @@
 CONFIG_DIR="$HOME/.config"
 NVIM_DIR="$CONFIG_DIR/nvim"
 
+npm install --global yarn -y
+
 # Setup config folder
 mkdir -p "$HOME/.config"
 
 # https://github.com/mpcarolin/kickstart.nvim 
 sudo add-apt-repository ppa:neovim-ppa/unstable -y
-sudo apt update
-sudo apt install make gcc ripgrep unzip git xclip neovim
+sudo apt update -y
+sudo apt install make gcc ripgrep unzip git xclip neovim -y
 
 # Needed for some plugins, like rest.nvim
-sudo apt install luarocks
+sudo apt install luarocks -y
 
 # clone the kickstart
 git clone https://github.com/mpcarolin/kickstart.nvim "$NVIM_DIR"
