794a41922149b4c420cc78cc78cf102535e17502
Michael Carolin <michael.carolin@i-360.com>
2026-04-13 07:13:18 -0700

    Add nvim-notify

---
+++ lua/custom/plugins/notify.lua
@@ -0,0 +1,11 @@
+return {
+  'rcarriga/nvim-notify',
+  config = function()
+    local notify = require 'notify'
+    notify.setup {
+      stages = 'fade_in_slide_out',
+      timeout = 3000,
+    }
+    vim.notify = notify
+  end,
+}
